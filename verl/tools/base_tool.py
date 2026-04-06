a50a9a8ea88ffa78d5ba2cdb3e49ca87e264822a
Andrew Zhu <me@andrew-zhu.com>
2026-04-06 13:11:54 +0900

    I don't need to see the tool every time thanks

--- verl/tools/base_tool.py
+++ verl/tools/base_tool.py
@@ -38,7 +38,7 @@ class BaseTool:
         self.tool_schema = tool_schema or self.get_openai_tool_schema()
         assert self.tool_schema is not None, "Tool schema is not set!"
         self.name = self.tool_schema.function.name
-        print(json.dumps(self.tool_schema.model_dump(exclude_unset=True, exclude_none=True), indent=2))
+        # print(json.dumps(self.tool_schema.model_dump(exclude_unset=True, exclude_none=True), indent=2))
 
     def get_openai_tool_schema(self) -> OpenAIFunctionToolSchema:
         return self.tool_schema
